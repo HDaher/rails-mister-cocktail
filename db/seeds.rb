e9321b88c4fa16c1567812c740f0c2107af72a3b
Hussein Daher <hussein.daher.24@gmail.com>
2019-05-14 16:54:18 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts 'Cleaning database...'
+Ingredient.destroy_all
+
+puts 'Creating ingredients...'
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+user_serialized = open(url).read
+user = JSON.parse(user_serialized)
+
+Ingredient.destroy_all
+
+10.times do
+  Ingredient.create(name: user['drinks'][rand(0..20)]["strIngredient1"])
+end
+
+puts "Finished!"
